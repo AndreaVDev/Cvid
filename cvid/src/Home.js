f3ca8629405ea4617f35def9e18b62ecdf380c2f
Andrea Verdura <andrea.verdura3@gmail.com>
2021-11-04 12:25:20 +0100

    Style Home page

--- cvid/src/Home.js
+++ cvid/src/Home.js
@@ -2,6 +2,21 @@ import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom"
 import Axios from "axios";
 import { v4 as uuidv4 } from "uuid";
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+import Button from '@mui/material/Button';
+import './styles/home.css'
+import Box from '@mui/material/Box';
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
+import ListItemButton from '@mui/material/ListItemButton';
+import ListItemIcon from '@mui/material/ListItemIcon';
+import ListItemText from '@mui/material/ListItemText';
+import Divider from '@mui/material/Divider';
+import { styled } from '@mui/material/styles';
+import Grid from '@mui/material/Grid';
 
 const Home = props => {
 	const [covidsData, setCovidData] = useState([]);
@@ -23,11 +38,12 @@ const Home = props => {
     fetchCovidData();
   }, []);
   
+  /*
   return (
     <div>
       {covidsData.map(covidData => {
         return (
-          <div key={covidData.id}>
+		<li key={covidData.id}>
             {covidData.country}
             <Link
               to={{
@@ -35,13 +51,45 @@ const Home = props => {
                 state: { covidsData: covidData }
               }}
             >
-              <button>View</button>
+      <Button variant="contained">View</Button>
             </Link>
-          </div>
+		  </li>
         );
       })}
     </div>
   );
+  */
+  return (
+  <Grid container
+  spacing={0}
+  direction="column"
+  alignItems="center"
+  justifyContent="center"
+  style={{ minHeight: '100vh' }}>
+    <List>
+      {covidsData.map(covidData => {
+        return (
+		<div>
+		<ListItem key={covidData.id}>
+            {covidData.country}
+            <Link className="link-details" 
+              to={{
+                pathname: `/view-details/${covidData.id}`,
+                state: { covidsData: covidData }
+              }}
+            >
+      <Button style={{marginLeft: '0.5em', height:'1.8em'}} variant="contained">View</Button>
+            </Link>
+		  </ListItem>
+		  <Divider />
+		  </div>
+        );
+      })}
+	  
+    </List>
+	</Grid>
+  );
+ 
 };
 
 export default Home;
--- cvid/src/ViewDetails.js
+++ cvid/src/ViewDetails.js
@@ -1,21 +1,24 @@
 import React from "react";
 import { useLocation, Link } from "react-router-dom";
+import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
 
 const ViewDetails = _ => {
   const { state } = useLocation();
 
   return (
     <div>
-        <div>
-          <div>
-            <strong>Name:</strong> {state.covidsData.country}{" "}
-          </div>
-          <div>
-            <strong>Today cases:</strong> {state.covidsData.todayCases}{" "}
-          </div>
-        </div>
+	<Typography variant="h1" component="h1">
+	{state.covidsData.country}{" "}
+	</Typography>
+	<Typography variant="h2" component="h2">
+	Today cases
+	</Typography>
+    <Typography variant="p" component="p">
+	{state.covidsData.todayCases}
+	</Typography>
       <Link to="/">
-        <button>Back</button>
+      <Button variant="contained">Back</Button>
       </Link>
     </div>
   );
