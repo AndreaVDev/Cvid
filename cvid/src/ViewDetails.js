7f6f8a20877e04bf18ba549b7ebffe4489bc7e4a
Andrea Verdura <andrea.verdura3@gmail.com>
2021-11-04 12:52:51 +0100

    Added flag to detail page

--- cvid/src/ViewDetails.js
+++ cvid/src/ViewDetails.js
@@ -2,6 +2,7 @@ import React from "react";
 import { useLocation, Link } from "react-router-dom";
 import Typography from '@mui/material/Typography';
 import Button from '@mui/material/Button';
+import './styles/viewdetails.css'
 
 const ViewDetails = _ => {
   const { state } = useLocation();
@@ -9,15 +10,19 @@ const ViewDetails = _ => {
   return (
     <div>
 	<Typography variant="h1" component="h1">
-	{state.covidsData.country}{" "}
+	Country: {state.covidsData.country}{" "} 
 	</Typography>
+	<img 
+      src={state.covidsData.countryInfo.flag}
+      alt="new"
+      />
 	<Typography variant="h2" component="h2">
 	Today cases
 	</Typography>
     <Typography variant="p" component="p">
 	{state.covidsData.todayCases}
 	</Typography>
-      <Link to="/">
+      <Link to="/" className="back-to-list">
       <Button variant="contained">Back</Button>
       </Link>
     </div>
